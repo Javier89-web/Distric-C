51ca7b65e5c38d6ca4c221580dfc30bf1eff28fa
Jajasaludos89 <brandon.pilco3401@utc.edu.ec>
2026-08-10 14:28:50 -0500

    Corregir carga de red vial en Render

--- Aplicaciones/proyectos/management/commands/cargar_red_vial.py
+++ Aplicaciones/proyectos/management/commands/cargar_red_vial.py
@@ -1,4 +1,5 @@
-from django.core.management.base import BaseCommand, call_command
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
 
 from Aplicaciones.proyectos.models import NodoMapa, TramoVial
 
